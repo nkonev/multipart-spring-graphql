e75e5f0cef4e7e57e1e02055cbe149f485a0e155
Nikita Konev <nikit.cpp@yandex.ru>
2023-07-09 19:37:39 +0400

    narrow accept in client

--- multipart-spring-graphql/src/main/java/name/nkonev/multipart/spring/graphql/client/MultipartGraphQlWebClient.java
+++ multipart-spring-graphql/src/main/java/name/nkonev/multipart/spring/graphql/client/MultipartGraphQlWebClient.java
@@ -10,7 +10,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
-import java.util.function.Consumer;
 
 import static name.nkonev.multipart.spring.graphql.client.MultipartBodyCreator.convertRequestToMultipartData;
 
@@ -19,8 +18,7 @@ public class MultipartGraphQlWebClient {
     private final WebClient webClient;
 
     private static final ParameterizedTypeReference<Map<String, Object>> MAP_TYPE =
-            new ParameterizedTypeReference<Map<String, Object>>() {
-            };
+            new ParameterizedTypeReference<Map<String, Object>>() {};
 
     public MultipartGraphQlWebClient(WebClient webClient) {
         Assert.notNull(webClient, "WebClient is required");
@@ -46,7 +44,7 @@ public class MultipartGraphQlWebClient {
 
     private Mono<MultipartResponseMapGraphQlResponse> makeMultipartRequest(WebClient.RequestBodySpec spec, MultipartClientGraphQlRequest request) {
         return spec.contentType(MediaType.MULTIPART_FORM_DATA)
-                .accept(MediaType.APPLICATION_JSON, MediaType.APPLICATION_GRAPHQL)
+                .accept(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromMultipartData(convertRequestToMultipartData(request)))
                 .retrieve()
                 .bodyToMono(MAP_TYPE)
