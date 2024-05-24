3a955e11f7ff83514bf2f0d5d59e3c17bfe737d4
Nikita Konev <nikit.cpp@yandex.ru>
2024-05-24 08:29:59 +0400

    fix autoconfiguration

--- multipart-spring-graphql/src/main/java/name/nkonev/multipart/springboot/graphql/client/MultipartGraphQlRestClientAutoconfiguration.java
+++ multipart-spring-graphql/src/main/java/name/nkonev/multipart/springboot/graphql/client/MultipartGraphQlRestClientAutoconfiguration.java
@@ -2,12 +2,16 @@ package name.nkonev.multipart.springboot.graphql.client;
 
 import name.nkonev.multipart.spring.graphql.client.webmvc.MultipartGraphQlRestClient;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
 @AutoConfiguration
+@AutoConfigureAfter(RestClientAutoConfiguration.class)
 @ConditionalOnClass(RestClient.class)
+@ConditionalOnBean(RestClient.Builder.class)
 @ConditionalOnProperty(value = "multipart.springboot.rest.client.enabled", matchIfMissing = true)
 public class MultipartGraphQlRestClientAutoconfiguration {
 
--- multipart-spring-graphql/src/main/java/name/nkonev/multipart/springboot/graphql/client/MultipartGraphQlWebClientAutoconfiguration.java
+++ multipart-spring-graphql/src/main/java/name/nkonev/multipart/springboot/graphql/client/MultipartGraphQlWebClientAutoconfiguration.java
@@ -2,12 +2,16 @@ package name.nkonev.multipart.springboot.graphql.client;
 
 import name.nkonev.multipart.spring.graphql.client.webflux.MultipartGraphQlWebClient;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @AutoConfiguration
+@AutoConfigureAfter(WebClientAutoConfiguration.class)
 @ConditionalOnClass(WebClient.class)
+@ConditionalOnBean(WebClient.Builder.class)
 @ConditionalOnProperty(value = "multipart.springboot.webflux.client.enabled", matchIfMissing = true)
 public class MultipartGraphQlWebClientAutoconfiguration {
 
